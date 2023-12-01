47801770233b6816f82d5056c8b71483b1d877f2
Iain Lynch <lynch.iain@gmail.com>
2023-12-01 16:49:43 +0000

    timed

--- day1.py
+++ day1.py
@@ -1,15 +1,16 @@
 from aocd.post import submit
+import pstats
+import cProfile
 
 def getFirstNum(string):
     for n, s in enumerate(string):
         if s.isdigit():
             return n
-        
     return len(string)
 
 def findTxtNum(string, reversed=False):
     txt_nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(len(string)):
+    for i in range(len(string)-5):
         s = string[i: i+5]
         for n, num in enumerate(txt_nums):
             if reversed:
@@ -18,11 +19,11 @@ def findTxtNum(string, reversed=False):
                 return str(n+1)
     return None
 
-if __name__ == "__main__":
+def main():
     with open("1.txt", "r") as f:
         data = f.read().splitlines()
         f.close()
-        
+
 #     data = '''two1nine
 # eightwothree
 # abcone2threexyz
@@ -30,6 +31,7 @@ if __name__ == "__main__":
 # 4nineeightseven2
 # zoneight234
 # 7pqrstsixteen'''.splitlines()
+
     result = 0
 
     for row in data:
@@ -55,3 +57,11 @@ if __name__ == "__main__":
     #     answer=result,
     #     session="53616c7465645f5f186878039ece58656365c1cc38126c25e88a97b152f885ef325d90ac8bda874c1190ed8f55318bdc6283e260ec9f17a3aacc76d326318206"
     # )
+
+if __name__ == "__main__":
+    with cProfile.Profile() as pr:
+        main()
+    
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats(10)
