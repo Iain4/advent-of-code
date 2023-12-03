27bcffc6c72336476f90901bf83f71387b265f7b
Iain Lynch <lynch.iain@gmail.com>
2023-12-03 17:28:26 +0000

    day 3 part b

--- day3.py
+++ day3.py
@@ -62,12 +62,58 @@ def part_1(data: list):# -> int:
     return total
 
 ### PART 2 ###
+def part_2(data: list) -> list:
+    # returns list of tuples of:
+    # (value, (star's index in data))
+    star_nums = []
+    star_pos = []
+    for n, row in enumerate(data):
+        nums = find_nums(row)
+        for num in nums:
+            star = check_for_star(data, n, num[1])
+            if star:
+                star_nums += [(num[0], star)]
+                star_pos += [star]
 
+    total = 0
+    for i in range(len(star_nums)-1):
+        num_1 = star_nums[i]
+        for j in range(i+1, len(star_nums)):
+            num_2 = star_nums[j]
+            if num_1[1] == num_2[1]:
+                total += num_1[0] * num_2[0]
+    return total
 
+def check_for_star(data: list, row_num: int, num_points: tuple) -> bool:
+    # checks if a symbol is around the number
+    start = num_points[0] - 1
+    if start < 0:
+        start = 0
+    end = num_points[1] + 1
+    if end > len(data[row_num]):
+        end = len(data[row_num])
+
+    if row_num == 0:
+        row_nums = range(row_num,row_num+2)
+    elif row_num+2 >= len(data[row_num]):
+        row_nums = range(row_num-1, len(data[row_num]))
+    else:
+        row_nums = range(row_num-1,row_num+2)
+
+    for i in row_nums:
+        row = data[i]
+        for j, char in enumerate(row):
+            if char == '*' and j in range(start,end):
+                return True, (i,j)
+    return False
 
 answer_1 = part_1(data)
 print(answer_1)
+
+answer_2 = part_2(data)
+print(answer_2)
+
 # submit(
 #     session=session,
-#     answer=answer_1
+#     answer=answer_2
 # )
