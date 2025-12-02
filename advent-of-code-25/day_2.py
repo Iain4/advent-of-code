bf24a8f76422ef6029b6c8d609cafe0f9231471f
Iain Lynch <lynch.iain@gmail.com>
2025-12-02 20:48:09 +0000

    day 2 fini

--- advent-of-code-25/day_2.py
+++ advent-of-code-25/day_2.py
@@ -1,6 +1,5 @@
 
-
-def id_invalid(id:int):
+def id_invalid_p1(id:int):
     id = str(id)
     length = len(id)
     
@@ -31,7 +30,7 @@ def part_1(ID_ranges):
                 id = 10 ** len(str(id))
                 continue
             
-            if id_invalid(id):
+            if id_invalid_p1(id):
                 invalid_id_sum += id
         
             id += 1
@@ -40,10 +39,34 @@ def part_1(ID_ranges):
             
 
 
-# check if the id has an even number of values
-# if it does, are both halves the same
-# if not an even length, should itterate to one so not testing hunners of useless values.
+def id_invalid_p2(id:str):
+    length = len(id)
+    half = int(length / 2)
+    
+    for i in range(1,half+1):
+        # seeing if that repeating number can fit
+        if length % i != 0:
+            continue
+        
+        repeats = int(length / i)
+        if id[:i] * repeats == id:
+            return True
+    
+    return False
 
+
+def part_2(ID_ranges):
+    invalid_id_sum = 0
+
+    for idr in ID_ranges:
+        start, stop = idr
+        start, stop = int(start), int(stop)+1
+
+        for id in range(start, stop):
+            if id_invalid_p2(str(id)):
+                invalid_id_sum += id
+
+    print(invalid_id_sum)
 
 
 if __name__ == "__main__":
@@ -52,4 +75,4 @@ if __name__ == "__main__":
 
     ID_ranges = [s.split("-") for s in data.split(",")]
 
-    part_1(ID_ranges)
+    part_2(ID_ranges)
